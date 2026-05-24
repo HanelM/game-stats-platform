3908714d0f37a70e9509152dd6b2d63ed556ce90
HanelM <misljimihanel@gmail.com>
2026-05-24 17:07:14 +0300

    Added Netlify CORS

--- src/main/java/com/gamestats/platform/config/CorsConfig.java
+++ src/main/java/com/gamestats/platform/config/CorsConfig.java
@@ -22,7 +22,8 @@ public class CorsConfig {
                 List.of(
                         "http://127.0.0.1:5500",
                         "http://localhost:5500",
-                        "http://localhost:63342"
+                        "http://localhost:63342",
+                        "https://game-stats-platform.netlify.app"
                 )
         );
 
